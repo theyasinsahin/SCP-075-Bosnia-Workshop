a11a6abcffca47fefca7bf0b9d0351f752bc3403
theyasinsahin <yasin248sahin@gmail.com>
2025-10-02 12:11:54 +0300

    initialize the frontend react app

---
+++ client/kitchen-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Kitchen App</h1>
+      <p>Welcome to the Kitchen App!</p>
+    </div>
+  );
+}
+
+export default App;
